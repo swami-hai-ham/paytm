a38d7e55a5e96ac6f5fb031bdca74d380120f0d7
swami-hai-ham <swamiatharva15104@gmail.com>
2024-06-23 16:44:30 +0530

    til store

---
+++ apps/user-app/app/api/user/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server"
+import prisma from "@repo/db/client";
+
+
+export const GET = async () => {
+    await prisma.user.create({
+        data: {
+            email: "asd",
+            name: "adsads"
+        }
+    })
+    return NextResponse.json({
+        message: "hi there"
+    })
+}
